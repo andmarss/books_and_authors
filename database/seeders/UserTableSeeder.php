a71286e5a40563a6351c91171e2f374c3d6cbdee
Андрей Марков <anrey-markov@mail.ru>
2022-10-18 10:48:35 +0300

    feat/seeders - сидеры книг и пользователей

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->command->info('Загрузка пользователей...');
+        User::query()->truncate();
+
+        User::factory()->count(50)->create();
+        $this->command->info('Пользователи загружены');
+    }
+}
