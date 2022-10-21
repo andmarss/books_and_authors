73ac535ddd2b6d4e9519062360f7467247dbbae7
Андрей Марков <anrey-markov@mail.ru>
2022-10-21 16:31:30 +0300

    feat/passport/auth - замена RequestGuard'а и TokenGuard'а на кастомные

--- config/app.php
+++ config/app.php
@@ -229,7 +229,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Laravel\Passport\Guards\TokenGuard'     => \App\Auth\TokenGuard::class,
+        'Illuminate\Auth\RequestGuard'     => \App\Auth\RequestGuard::class,
     ],
 
 ];
