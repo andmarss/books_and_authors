a13b353f37ed497589593a05916047b52ed1ba6d
Андрей Марков <anrey-markov@mail.ru>
2022-10-25 19:01:21 +0300

    feat/front/js/css - js/css файлы

--- resources/js/components.js
+++ resources/js/components.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import LoginForm from './components/Auth/Login.vue';
 import UserLoginLogout from './components/Auth/UserLoginLogout.vue';
+import RegisterForm from './components/Auth/Register.vue';
 
 Vue.component('login-form', LoginForm);
 Vue.component('user-login-logout', UserLoginLogout);
+Vue.component('register-form', RegisterForm);
